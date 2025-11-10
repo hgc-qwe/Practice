61fc1b06eae7c01303b81f55f87b589952e8a856
hgc-qwe <1410919772@qq.com>
2025-11-10 18:40:35 +0800

    11.10

---
+++ 1/1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    long long n;
+    while(scanf("%lld",&n)!=EOF){
+        long long A1=n/2+n/5+n/11+n/13;
+        long long A2=n/10+n/22+n/143+n/55+n/65+n/26;
+        long long A3=n/110+n/130+n/286+n/715;
+        long long A4=n/1430;
+
+        long long t=A1-A2+A3-A4;
+        long long result=n-t;
+
+        printf("%lld\n",result);
+    }
+    
+    return 0;
+}
